ad20286a18e00168a4335acb7e46c25cf5b47df1
iTomasi <duclosiw@gmail.com>
2021-06-10 12:19:52 -0400

    asdasd

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,6 @@
-import MetaDecorator from "../components/MetaDecorator";
-
 const Custom404 = () => {
   return (
     <>
-      <MetaDecorator
-        title="Page not found"
-        description="please check url"
-        imageAlt="asdasd"
-        imageUrl="https://cdn.dribbble.com/users/3693559/screenshots/11398071/404_not_found.png"
-      />
       <h1>This is a 404 page custom</h1>
     </>
   );
--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,14 +1,6 @@
-import MetaDecorator from "../components/MetaDecorator";
-
 const Error = () => {
   return (
     <>
-      <MetaDecorator
-        title="Page not found"
-        description="please check url"
-        imageAlt="asdasd"
-        imageUrl="https://cdn.dribbble.com/users/3693559/screenshots/11398071/404_not_found.png"
-      />
       <h1>ERROR PAGE</h1>
     </>
   );
