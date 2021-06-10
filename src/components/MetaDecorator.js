101033c8916f310e18f6892b8a43717521e0d7c6
Daniel <bdcaldera@gmail.com>
2021-06-10 12:12:03 -0500

    added meta tags to show twitter og

--- src/components/MetaDecorator.js
+++ src/components/MetaDecorator.js
@@ -1,19 +1,22 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 const MetaDecorator = ({ title, description, imageUrl, imageAlt }) => {
   return (
     <Head>
       <title>{title}</title>
 
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:url" content="https://tomasportfolio.netlify.app/" />
-      <meta property="twitter:card" content={description} />
-      <meta property="twitter:image:alt" content={imageAlt} />
+      <meta name='description' content={description} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content={'app'} />
+      <meta property='og:image' content={imageUrl} />
+      <meta property='og:url' content='https://tomasportfolio.netlify.app/' />
+      <meta property='twitter:card' content={'app'} />
+      <meta property='twitter:image' content={imageUrl} />
+      <meta property='twitter:image:alt' content={imageAlt} />
+      <meta property='twitter:text:title' content={title}></meta>
     </Head>
-  );
-};
+  )
+}
 
-export default MetaDecorator;
+export default MetaDecorator
